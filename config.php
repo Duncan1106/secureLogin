2b7e5b57fc01ce2290ab9ef6e5211dd175822f02
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-04-25 10:11:10 +0200

    Update config.php
    added pepper

--- config.php
+++ config.php
@@ -5,10 +5,13 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'test');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
+//all_pepper 
+$all_pepper = "a8G3eQLHpFeSeIhmDbhzhTRUEB1U6DZCs1M9SnZMl6WDVQfcY1E3z26oXX3tkHfUa8G3eQLHpFeSeIhmDbhzhTRUEB1U6DZCs1M9SnZMl6WDVQfcY1E3z26oXX3tkHfUa8G3eQLHpFeSeIhmDbhzhTRUEB1U6DZCs1M9SnZMl6WDVQfcY1E3z26oXX3tkHfU";
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
