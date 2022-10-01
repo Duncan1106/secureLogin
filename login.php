0dad27028dad479266e5690ee4eca909ce588f1a
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-10-01 20:22:09 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: welcome.php");
     exit;
 }
@@ -16,28 +16,28 @@ $username_ = $password = "";
 $username_err = $password_err = "";
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
  
     // Check if username is empty
-    if(empty(trim($_POST["username"]))) {
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter username.";
     } else {
         $username_ = trim($_POST["username"]);
     }
     
     // Check if password is empty
-    if(empty(trim($_POST["password"]))) {
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter your password.";
     } else {
         $password = trim($_POST["password"]);
     }
     
     // Validate credentials
-    if(empty($username_err) && empty($password_err)) {
+    if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)) {
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
@@ -45,15 +45,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             $param_username = $username_;
             
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)) {
+            if (mysqli_stmt_execute($stmt)) {
                 // Store result
                 mysqli_stmt_store_result($stmt);
                 
                 // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1) {                    
+                if (mysqli_stmt_num_rows($stmt) == 1) {                    
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
+                    if (mysqli_stmt_fetch($stmt)){
                         // Salt and pepper the password
                         $salted = password_hash($username, PASSWORD_DEFAULT);
                         $peppered = $all_pepper;
@@ -61,7 +61,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                         $passwordsp = password_hash($password_temp, PASSWORD_DEFAULT);
                         $hash_password = password_hash($passwordsp, PASSWORD_DEFAULT);
                      
-                        if(password_verify($hash_password, $hashed_password)) {
+                        if (password_verify($hash_password, $hashed_password)) {
                             // Password is correct, so start a new session
                             session_start();
                             
