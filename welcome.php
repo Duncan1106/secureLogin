1c89f007387b3402d6b389400fad2eefa40b2344
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-10-04 14:02:47 +0200

    Add files via upload

--- welcome.php
+++ welcome.php
@@ -36,18 +36,18 @@ require_once "config.php";
                       $sql = "SELECT id, firstname, lastname, username, email, date FROM users WHERE username = '".$_SESSION['username']."'";
                       $result = mysqli_query($link, $sql);
                       if (mysqli_num_rows($result) > 0) {
-                        // output data of each row
-                        while($row = mysqli_fetch_assoc($result)) {
-                            echo "<th colspan='2'>" . $_SESSION['username'] . "`s Dashboard" . "</th>".
-                                "<tr><td>" . "User ID: " . "</td>" . "<td>" . $row["id"]. "</td></tr>" .
-                                "<tr><td>" . "First Name: " . "</td>" . "<td>" . $row["firstname"]. "</td></tr>" .
-                                "<tr><td>" . "Last Name: " .  "</td>" . "<td>" . $row["lastname"]. "</td></tr>" .
-                                "<tr><td>" . "User Name: " .  "</td>" . "<td>" . $row["username"] . "</td></tr>" .
-                                "<tr><td>" . "Email: " .  "</td>" . "<td>" . $row["email"] . "</td></tr>" .
-                                "<tr><td>" . "Member since: " . "</td>" . "<td>" .  $row['date'] . "</td></tr>";
-                        }
+                          // output data of each row
+                          while($row = mysqli_fetch_assoc($result)) {
+                              echo "<th colspan='2'>" . $_SESSION['username'] . "`s Dashboard" . "</th>".
+                                  "<tr><td>" . "User ID: " . "</td>" . "<td>" . $row["id"]. "</td></tr>" .
+                                  "<tr><td>" . "First Name: " . "</td>" . "<td>" . $row["firstname"]. "</td></tr>" .
+                                  "<tr><td>" . "Last Name: " .  "</td>" . "<td>" . $row["lastname"]. "</td></tr>" .
+                                  "<tr><td>" . "User Name: " .  "</td>" . "<td>" . $row["username"] . "</td></tr>" .
+                                  "<tr><td>" . "Email: " .  "</td>" . "<td>" . $row["email"] . "</td></tr>" .
+                                  "<tr><td>" . "Member since: " . "</td>" . "<td>" .  $row['date'] . "</td></tr>";
+                          }
                       } else {
-                      echo "Oops! Something went wrong. Please try again later.";
+                          echo "Oops! Something went wrong. Please try again later.";
                       }
                       ?>
                   </table>
