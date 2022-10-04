b01c5ce6dce74e3ee0da0d55caec3d3ae51c237a
Dark1106 <73903126+Duncan1106@users.noreply.github.com>
2022-10-04 13:51:29 +0200

    Update welcome.php
    added dashboard of user

--- welcome.php
+++ welcome.php
@@ -7,6 +7,10 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.html");
     exit;
 }
+
+require_once "config.php";
+
+
 ?>
 
 <!DOCTYPE html>
@@ -16,16 +20,45 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
+        body{ font: 14px sans-serif; text-align: center; vertical-align: middle}
+        table{ margin:auto; display: table; text-align: left; vertical-align: middle; table-layout: auto;}
+        table, th, td, tr{ border:1px solid; padding: 10px; text-align: center;}
     </style>
 </head>
 <body>
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    </div>
+        <div>
+            <div>
+                <div class="table-responsive">
+                  <table>
+                      <?php
+                      $sql = "SELECT id, firstname, lastname, username, email, date FROM users WHERE username = '".$_SESSION['username']."'";
+                      $result = mysqli_query($link, $sql);
+                      if (mysqli_num_rows($result) > 0) {
+                      // output data of each row
+
+                          while($row = mysqli_fetch_assoc($result)) {
+                              echo "<th colspan='2'>" . $_SESSION['username'] . "`s Dashboard" . "</th>".
+                                   "<tr><td>" . "User ID: " . "</td>" . "<td>" . $row["id"]. "</td></tr>" .
+                                   "<tr><td>" . "First Name: " . "</td>" . "<td>" . $row["firstname"]. "</td></tr>" .
+                                   "<tr><td>" . "Last Name: " .  "</td>" . "<td>" . $row["lastname"]. "</td></tr>" .
+                                   "<tr><td>" . "User Name: " .  "</td>" . "<td>" . $row["username"] . "</td></tr>" .
+                                   "<tr><td>" . "Email: " .  "</td>" . "<td>" . $row["email"] . "</td></tr>" .
+                                   "<tr><td>" . "Member since: " . "</td>" . "<td>" .  $row['date'] . "</td></tr>";
+                          }
+                      } else {
+                      echo "Oops! Something went wrong. Please try again later.";
+                      }
+                      ?>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div">
     <p>
-        <a href="reset-password.html" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.html" class="btn btn-danger">Sign Out of Your Account</a>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
 </body>
 </html>
